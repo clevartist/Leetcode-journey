f930882b461b0b2d72de7d77bd6a4bb00b6ad962
Clev <105460616+clevartist@users.noreply.github.com>
2024-06-24 12:12:35 +0300

    Update decompressRLElist.c

--- C/decompressRLElist.c
+++ C/decompressRLElist.c
@@ -1,9 +1,40 @@
-// Below is the "sketch"
-// so never mind
-
 #include <stdio.h>
 #include <stdlib.h>
 
+// Final code
+int* decompressRLElist(int* nums, int numsSize, int* returnSize) {
+    
+    int total_size = 0;
+    for(int i = 0; i < numsSize; i += 2) {
+        total_size += nums[i];
+    }
+
+    // allocate memory
+    int *alloc = (int *)malloc(total_size * sizeof(int));
+    if(alloc == NULL){
+        *returnSize = 0;
+        return NULL;
+    }
+
+    // apply values to the allocated memory
+    int current = 0;
+    for(int k = 0; k < numsSize; k+=2) {
+        int freq = nums[k];
+        int val = nums[k + 1];
+
+        for(int p = 0; p < freq; p++) {
+            alloc[current++] = val;
+        }
+    }
+
+    *returnSize = total_size;
+    return alloc;
+}
+
+
+
+// Below is the "sketch"
+// so never mind
 int main() {
     int nums[] = {1,2,3,4,5,6,7,8,9,10};
     int numsSize = sizeof(nums) / sizeof(nums[0]);
